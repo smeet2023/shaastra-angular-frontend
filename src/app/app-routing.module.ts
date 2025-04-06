c92541fa7f635f11f5139532c768e8c36e154902
Smeet A <smeetadeshra@gmail.com>
2025-04-06 13:36:06 +0530

    commit-2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginAdminComponent } from './auth/login-admin/login-admin.component';
+import { LoginParticipantComponent } from './auth/login-participant/login-participant.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login/admin', pathMatch: 'full' },
+  { path: 'login/admin', component: LoginAdminComponent },
+  { path: 'login/participant', component: LoginParticipantComponent },
+  // ...other routes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,6 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginAdminComponent } from './login-admin/login-admin.component';
 import { LoginParticipantComponent } from './login-participant/login-participant.component';
